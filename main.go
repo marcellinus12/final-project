1c7a6762dcccda26073dbbfcf572f1d95e5e1a94
bob <marcellinus.cg@gmail.com>
2024-03-22 23:00:21 +0700

    fix

--- main.go
+++ main.go
@@ -9,10 +9,13 @@ import (
 
 func main() {
 	database.StartDB()
+	r := router.StartApp()
 	PORT := os.Getenv("PORT")
 
-	fmt.Sprintln("server start at :", PORT)
-	router.StartApp().Run(PORT)
+	fmt.Println("server start at :" + PORT)
+	if err := r.Run(":" + PORT); err != nil {
+		panic("panic failed to start")
+	}
 
 	// r := router.StartApp()
 	// r.Run("8080")
